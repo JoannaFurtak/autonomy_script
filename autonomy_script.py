e8c735c80afe8c733641a03556229b62f4cb4c7a
JoannaFurtak <joannafurtak5@gmail.com>
2026-08-16 22:27:18 +0200

    one interface (gps only)

--- autonomy_script.py
+++ autonomy_script.py
@@ -8,10 +8,8 @@ import math
 
 actual_covariance = [999.0]
 actual_position = [0.0, 0.0, 0.0]
-
-def create_window(stdscr):
-    stdscr.clear()
-    stdscr.box()
+covariance_status = "none"
+gps_ready = False
 
 def gps_callback(msg):
     if len(msg.position_covariance) > 0:
@@ -21,37 +19,19 @@ def gps_callback(msg):
         actual_position[1] = msg.longitude
         actual_position[2] = msg.altitude
 
-def gps(stdscr):
-    create_window(stdscr)
-    stdscr.addstr(2, 2, "Step 1 - gps startup, q to quit")
+def gps():
     subprocess.Popen(["roslaunch", "sirius_navigation", "gnss.launch"],
                      stdout=subprocess.DEVNULL,
                      stderr=subprocess.DEVNULL
     )
     gps_sub = rospy.Subscriber('gps/fix', NavSatFix, gps_callback)
 
-    stdscr.timeout(100) 
-    ready = False
+    global covariance_status, gps_ready
+    covariance_status = "waiting for data"
+    gps_ready = False
 
-    while True:
 
-        if actual_covariance[0] <= 0.002 and not ready:
-            ready = True
-            gps_sub.unregister()
 
-        if ready:
-            stdscr.addstr(6, 2, 'covariance is ok, press s to proceed to slam')
-        else:
-            stdscr.addstr(6, 2, f'covariance: {actual_covariance[0]}')
-
-        try:
-            key = stdscr.getkey().lower()
-            if ready and key == 's':
-                return 's'
-            elif key == 'q':
-                return
-        except curses.error:
-            pass
 
 last_pos = [None, None]
 driven_distance = [0.0]
@@ -206,35 +186,89 @@ def navigation(stdscr):
             pass
 
 def main(stdscr):
-    create_window(stdscr)
-    stdscr.timeout(100)
-    state = None
 
+    #wsteone ustawienie
+    global covariance_status, actual_covariance, gps_ready, gps_sub
+    curses.curs_set(0) #niema myszkiiii
+    stdscr.timeout(100) #10razy na sek petla
+
+    #kroki
+    options = [
+        "1. GPS startup",
+        "2. SLAM",
+        "3. localization",
+        "4. mapping",
+        "5. navigation",
+        "4. quit programm"
+    ]
+    aktualny_wiersz = 0
+
+    #statusy odpalenia
+    status_gps = "[ none ]"
+    status_slam = "[ none ]"
+
+    #petla dzialania funkcji i rysowania ekranu
     while True:
-        if state == None:
-            try:
-                state = stdscr.getkey().lower()
-            except curses.error():
-                continue
+        stdscr.clear()
 
-        try:
-            if state == 'g':
-                state = gps(stdscr)
-            elif state == 's':
-                state = slam(stdscr)
-            elif state == 'l':
-                state = localization(stdscr)
-            elif state == 'm':
-                state = mapping(stdscr)
-            elif state == 'n':
-                state = navigation(stdscr)
-            elif state == 'q':
-                return
+        #1 GPS 
+        if gps_sub is not None and not gps_ready:
+            if actual_covariance[0] != 999.0: 
+                if actual_covariance[0] <= 0.002:
+                    gps_ready = True
+                    gps_sub.unregister()
+                    covariance_status = "ready"
+                else:
+                    covariance_status = f"{round(actual_covariance[0], 4)}"
+
+        #RYSOWANIE CHECKLISTY
+        stdscr.addstr(1, 2, "AUTONOMY STARTUP", curses.A_BOLD)
+        stdscr.addstr(3, 2, f"status gps = [{covariance_status}]")
+        stdscr.addstr(4, 2, "status slam = []")
+        stdscr.addstr(5, 2, "status localization = []")
+        stdscr.addstr(6, 2, "status mapping = []")
+        stdscr.addstr(7, 2, "status navigation = []")
+        stdscr.addstr(8, 2, "-" * 40)
+        
+        #menu do odpalania funkcji
+        stdscr.addstr(10, 2, "functions:", curses.A_BOLD)
+        for index, option_txt in enumerate(options):
+            x = 4
+            y = 12 + index
+            
+            if index == aktualny_wiersz:
+                #podswietlenie
+                stdscr.addstr(y, x, f"> {option_txt} <", curses.A_REVERSE)
             else:
-                state == None
-        except curses.error():
-            pass
-                
+                stdscr.addstr(y, x, f"  {option_txt}  ")
+
+        stdscr.refresh()
+
+        #obsluga klawiszy
+        klawisz = stdscr.getch()
+
+        if klawisz == curses.KEY_UP and aktualny_wiersz > 0:
+            aktualny_wiersz -= 1
+        elif klawisz == curses.KEY_DOWN and aktualny_wiersz < len(options) - 1:
+            aktualny_wiersz += 1
+        elif klawisz in [curses.KEY_ENTER, 10, 13]: # Enter
+            
+            #logiki dla wierszy
+            if aktualny_wiersz == 0:
+                gps()
+            elif aktualny_wiersz == 1:
+                slam()
+            elif aktualny_wiersz == 2:
+                localization(stdscr)
+                status_slam = "[ active ]"
+            elif aktualny_wiersz == 3:
+                mapping(stdscr)
+                status_slam = "[ active ]"
+            elif aktualny_wiersz == 4:
+                status_nav = "[active]"
+                navigation(stdscr)
+            elif aktualny_wiersz == 5:
+                break
 
 if __name__ == '__main__':
     curses.wrapper(main)
