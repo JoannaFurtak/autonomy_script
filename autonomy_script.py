a81912c513525b95242f24bbd0c69f2b8548d92a
JoannaFurtak <joannafurtak5@gmail.com>
2026-08-19 16:09:36 +0200

    redesigned ui using pick

--- autonomy_script.py
+++ autonomy_script.py
@@ -1,16 +1,17 @@
 import curses
 import subprocess
 import rospy
-from pick import Option, pick
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
 import math
+import yaml
 
 actual_covariance = [999.0]
 actual_position = [0.0, 0.0, 0.0]
 covariance_status = "none"
 gps_ready = False
 gps_sub = None
+status_gps = "inactive"
 
 def gps_callback(msg):
     if len(msg.position_covariance) > 0:
@@ -33,13 +34,13 @@ def gps():
 
 
 
-status_slam = "[inactive]"
+status_slam = "inactive"
 slam_launched = False
 odom_sub = None
 last_pos = [None, None]
 driven_distance = [0.0]
 
-def odometry_callback(msg, stdscr):
+def odometry_callback(msg):
     current_x = msg.pose.pose.position.x
     current_y = msg.pose.pose.position.y
 
@@ -57,141 +58,93 @@ def odometry_callback(msg, stdscr):
         last_pos[1] = current_y
 
 
+#sciezka do yamla!!!! potrzebna
 def slam(stdscr):
+    global status_slam, slam_launched, odom_sub, actual_position
+
     curses.endwin()
+
+    title = "Pick a reference point for SLAM: "
+    options = ["1. current pos", "2. itc" ]
+    chosen_point, index = pick(options, title)
     
-    stdscr.addstr(2, 2, "Step 2 - slam startup, q to quit")
-    slam_launched = False
+    if index == 0:
+        # Zawsze indeks 0 to nasz GPS
+        rospy.set_param('latitude', actual_position[0])
+        rospy.set_param('longitude', actual_position[1])
+        rospy.set_param('altitude', actual_position[2])
+        
+        status_slam = "[active (pos from gps)-> drive for 40m]"
+        
+    elif index == 1:
+        slam_params_path =  "hhhh"
+        with open(slam_params_path, 'r') as file:
+            coords = yaml.safe_load(file)
 
-    while True:
+        yaml_lat = coords['latitude']
+        yaml_lon = coords['longitude']
+        yaml_alt = coords['altitude']
+        
+        rospy.set_param('latitude', yaml_lat)
+        rospy.set_param('longitude', yaml_lon)
+        rospy.set_param('altitude', yaml_alt)
+        
+        status_slam = f"[ active (yaml) -> drive for 40m ]"
 
-        if not slam_launched:
-            create_window(stdscr)
-            stdscr.addstr(4, 2, "choose a reference point:")
-            stdscr.addstr(5, 4, " 1 - current position")
-            stdscr.addstr(6, 4, "2 - saved position")
+    subprocess.Popen(["roslaunch", "sirius_spectacularai", "slam.launch"],
+                     stdout=subprocess.DEVNULL,
+                     stderr=subprocess.DEVNULL)
+                     
+    odom_sub = rospy.Subscriber('slam/global_odometry', Odometry, odometry_callback)
+    
+    slam_launched = True
+    stdscr.clear()
+    stdscr.refresh()
 
-        elif slam_launched:
-            if driven_distance[0] >= 40.0:
-                stdscr.addstr(14, 2, "drived 40m, press l")
-                stdscr.refresh()
 
-
-        try:
-            key = stdscr.getkey().lower()
-
-            if key == 'q':
-                return #wyjscie
-            
-            if not slam_launched:
-                # pkt referencyjny == gps
-                if key == '1':
-                    create_window(stdscr)
-                    rospy.set_param('NAZWA_PARAMETRU_PKT', actual_position)
-                    stdscr.addstr(10, 2, "go shake the rover, then drive for 40m")
-                    stdscr.addstr(12, 2, "next press l -> localization")
-                    subprocess.Popen(["roslaunch", "sirius_spectacularai", "slam.launch"],
-                                        stdout=subprocess.DEVNULL,
-                                        stderr=subprocess.DEVNULL
-                        )
-                    odom_sub = rospy.Subscriber('slam/global_odometry', Odometry, odometry_callback, callback_args=stdscr)
-                    slam_launched = True
-
-                # pkt referencyjny == wybierasz
-                elif key == '2':
-                    create_window(stdscr)
-                    points = ["p1", "p2"]
-                    title = "pick a referenece point"
-                    stdscr.timeout(-1)
-                    curses.endwin() #???
-                    chosen_point, index = pick(points, title)
-                    stdscr.clear()
-                    stdscr.refresh()
-                    try:
-                        coordinates = rospy.get_param(chosen_point)
-                        rospy.set_param("nazsa paramwetu pkt", coordinates)
-                        stdscr.timeout(100)
-                        stdscr.addstr(12, 2, "go shake the rover, then drive for 40m")
-                        stdscr.addstr(14, 2, "next press l -> localization")
-                        subprocess.Popen(["roslaunch", "sirius_spectacularai", "slam.launch"],
-                                                                stdout=subprocess.DEVNULL,
-                                                                stderr=subprocess.DEVNULL
-                                        )
-                        odom_sub = rospy.Subscriber('slam/global_odometry', Odometry, odometry_callback, callback_args=stdscr)
-                        slam_launched = True
-                    except KeyError:
-                        stdscr.addstr(14, 2, "chosen point doesn't exist" )
-
-            else:
-                if key == 'l' and driven_distance[0] >= 40.0:
-                    odom_sub.unregister()
-                    return 'l'
-
-        except curses.error:
-            pass
-
-def localization(stdscr):
-    create_window(stdscr)
-    stdscr.timeout(100)
-    stdscr.addstr(2, 2, "Step 3 - localization startup, q to quit")
+status_localization = "inactive"
+def localization():
+    global status_localization
     subprocess.Popen(["roslaunch", "sirius_navigation", "localization.launch"],
                      stdout=subprocess.DEVNULL,
                      stderr=subprocess.DEVNULL
                     )
-    while True:
-        try:
-            key = stdscr.getkey().lower()
+    status_localization = "active"
 
-            if key == 'q':
-                return
-            elif key == 'm':
-                return 'm'
+status_mapping = "inactive"
+mapping_launched = False
 
-        except curses.error:
-            pass
-
-def mapping(stdscr):
-    create_window(stdscr)
-    stdscr.timeout(100)
-    stdscr.addstr(2, 2, "Step 4 - mapping startup, q to quit")
+def mapping():
+    global status_mapping
     subprocess.Popen(["roslaunch", "sirius_mapping", "sirius_mapping.launch"],
                      stdout=subprocess.DEVNULL,
                      stderr=subprocess.DEVNULL
                     )
-    while True:
-        try:
-            key = stdscr.getkey().lower()
 
-            if key == 'q':
-                return
-            elif key == 'n':
-                return 'n'
+    status_mapping = "[ active ]"
+    global mapping_launched
+    mapping_launched = True
 
-        except curses.error:
-            pass
 
-def navigation(stdscr):
-    create_window(stdscr)
-    stdscr.timeout(100)
-    stdscr.addstr(2, 2, "Krok 5 - naviagtion startup, q to quit")
+status_navigation = "inactive"
+
+def navigation():
+    global status_navigation
     subprocess.Popen(["roslaunch", "sirius_navigation", "navigation.launch"],
                      stdout=subprocess.DEVNULL,
                      stderr=subprocess.DEVNULL
                     )
-    while True:
-        try:
-            key = stdscr.getkey().lower()
-
-            if key == 'q':
-                return
-
-        except curses.error:
-            pass
+    
+    status_navigation = "active"
 
 def main(stdscr):
+    global covariance_status, actual_covariance, gps_ready, gps_sub, status_gps
+    status_slam, slam_launched, driven_distance, 
+    status_localization,
+    status_mapping, mapping_launched,
+    status_navigation
 
     #wsteone ustawienie
-    global covariance_status, actual_covariance, gps_ready, gps_sub
     curses.curs_set(0) #niema myszkiiii
     stdscr.timeout(100) #10razy na sek petla
 
@@ -205,10 +158,6 @@ def main(stdscr):
         "4. quit programm"
     ]
     aktualny_wiersz = 0
-
-    #statusy odpalenia
-    status_gps = "[ none ]"
-    status_slam = "[ none ]"
 
     #petla dzialania funkcji i rysowania ekranu
     while True:
@@ -228,9 +177,9 @@ def main(stdscr):
         stdscr.addstr(1, 2, "AUTONOMY STARTUP", curses.A_BOLD)
         stdscr.addstr(3, 2, f"status gps = [{covariance_status}]")
         stdscr.addstr(4, 2, f"status slam = [{status_slam}]")
-        stdscr.addstr(5, 2, "status localization = []")
-        stdscr.addstr(6, 2, "status mapping = []")
-        stdscr.addstr(7, 2, "status navigation = []")
+        stdscr.addstr(5, 2, f"status localization = [{status_localization}]")
+        stdscr.addstr(6, 2, f"status mapping = [{status_mapping}]")
+        stdscr.addstr(7, 2, f"status navigation = [{status_navigation}]")
         stdscr.addstr(8, 2, "-" * 40)
         
         #menu do odpalania funkcji
@@ -259,21 +208,32 @@ def main(stdscr):
             #logiki dla wierszy
             if aktualny_wiersz == 0 and gps_sub is None:
                 gps()
+
             elif aktualny_wiersz == 1:
                 if not gps_ready:
-                    status_slam = ["gps not active"]
+                    status_slam = "gps not active"
                 else:
-                    curses.endwin
+                    slam(stdscr)
 
             elif aktualny_wiersz == 2:
-                localization(stdscr)
-                status_slam = "[ active ]"
+                if not slam_launched:
+                    status_localization = "error start slam first"
+                elif driven_distance[0] < 40.0:
+                    status_localization = f"drive 40m, now: {round(driven_distance[0], 1)}"
+                else:
+                    localization(stdscr)
+            
             elif aktualny_wiersz == 3:
-                mapping(stdscr)
-                status_slam = "[ active ]"
+                if status_localization != "active":
+                    status_mapping = "error: start localization first"
+                else:
+                    mapping()
+
             elif aktualny_wiersz == 4:
-                status_nav = "[active]"
-                navigation(stdscr)
+                if status_mapping != "active":
+                    status_navigation = "error: start mapping first"
+                else:
+                    navigation()
             elif aktualny_wiersz == 5:
                 break
 
